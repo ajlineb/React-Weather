5a356a0ff30c167bdf0c4be050daa4fd69636301
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-03 23:35:47 -0400

    setting up the base code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {
+  HashRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router basename="/weather_dashboard">
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
