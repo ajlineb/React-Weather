d37ec99e9bccb2644ca9e48b2d9664999aa8e26e
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-06 22:54:40 -0400

    setting up the main content in the center its layout and position

--- src/components/Sidetabs.js
+++ src/components/Sidetabs.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function Sidetabs() {
   return (
-    <div className="flex flex-col p-3 gap-20 flex-initial shadow-lg bg-slate-800 w-28 pt-10">
+    <div className="flex flex-col p-3 gap-20 flex-initial shadow-lg bg-slate-800 w-28 pt-10 ">
       <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
         tab 1 Home
       </div>
@@ -10,10 +10,10 @@ export default function Sidetabs() {
         tab 2 Maps
       </div>
       <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
-        tab 3
+        linkdin
       </div>
       <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
-        tab 4
+        Github
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,18 @@ export default function Home() {
   //not for this page but header tabs
   //should contain a map tab at least kinda want to try it out
 
+  //for my own mental sanity...
+  //grid grid-cols-3 is similar to bootstrap with a row and three columns
+  //to add gutters you will need gap-x for the gutter amounts
+
   return (
     <div className="h-screen pb-16 flex flex-row">
       <Sidetabs />
-      <div></div>
+      <div className="flex-1 grid grid-col-3 grid-flow-col gap-4 p-5">
+        <div className="bg-slate-400">ree</div>
+        <div className="bg-slate-500">ree</div>
+        <div className="bg-slate-600">ree</div>
+      </div>
     </div>
   );
 }
