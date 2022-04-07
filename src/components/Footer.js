ae5ff3d42574401ef0bab7e536005840a159afc0
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-06 21:53:06 -0400

    search bar and button are functioning correctly now with changes to the window

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 font-bold text-center drop-shadow">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 font-bold text-center ">
       <h1 className="">Created with ❤️ By Anthony Linebaugh</h1>
       <h1>© 2022</h1>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,17 @@ export default function Header() {
   return (
     <div className="flex flex-row flex-initial shadow-lg">
       <a href="/weather_dashboard">
-        <img src="https://picsum.photos/75" alt="site logo"></img>
+        <img src="https://picsum.photos/100" alt="site logo"></img>
       </a>
-      <form className="city-search basis-1/4  pl-5 relative ">
+      <form className="city-search basis-1/4 flex justify-center items-center ">
         <input
-          className="px-3 py-2 w-1/2 absolute top-1/2 -translate-y-1/2 border-2 rounded-md border-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring-1 focus:ring-indigo-700 placeholder:italic"
+          className="px-3 py-2 w-1/2 border-2 rounded-l-md border-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring-1 focus:ring-indigo-700 placeholder:italic shrink h-10"
           type="text"
           placeholder="Search a City..."
           name="search"
         ></input>
         <button
-          className="px-3 py-2 text-white font-bold rounded-lg bg-indigo-700 absolute top-1/2 right-1/4 -translate-y-1/2 hover:bg-violet-700 active:bg-violet-700 focus:outline-none focus:ring focus:ring-indigo-700"
+          className="px-3 py-2 text-white font-bold rounded-r-lg bg-indigo-700 hover:bg-violet-700 active:bg-violet-700 focus:outline-none focus:ring focus:ring-indigo-700 shrink h-10"
           type="submit"
         >
           Search!
