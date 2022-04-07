eb06203eee5290c1a0e185a1bdaeab28cd692aba
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-06 22:31:58 -0400

    updated the tabs centering inside

--- src/components/Sidetabs.js
+++ src/components/Sidetabs.js
@@ -3,17 +3,17 @@ import React, { useState, useEffect } from "react";
 export default function Sidetabs() {
   return (
     <div className="flex flex-col p-3 gap-20 flex-initial shadow-lg bg-slate-800 w-28 pt-10">
-      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md">
+      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
         tab 1 Home
       </div>
-      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md">
+      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
         tab 2 Maps
       </div>
-      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md">
-        tab 3 somthing
+      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
+        tab 3
       </div>
-      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md">
-        tab 4 somthing
+      <div className="h-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md flex justify-center items-center">
+        tab 4
       </div>
     </div>
   );
