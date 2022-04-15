bef1bcd1fa4c273ce9ee9c5061adca08162457bb
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-15 19:43:35 -0400

    setting up the home page to recieve the data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidetabs from "../components/Sidetabs";
+import ApiSearch from "../utils/apiSearch";
 
 export default function Home() {
   //brainstorm ideas that this home page should have on it..
@@ -12,6 +13,36 @@ export default function Home() {
   //for my own mental sanity...
   //grid grid-cols-3 is similar to bootstrap with a row and three columns
   //to add gutters you will need gap-x for the gutter amounts
+  const [data, setData] = useState(null);
+
+  //current Day states
+  const [currentDay, setCurrentDay] = useState("Current Day");
+  const [currentDayWeatherImage, setCurrentDayWeatherImage] =
+    useState("Animated image");
+  const [currentDayWeather, setCurrentDayWeatehr] =
+    useState("Weather that day");
+  const [currentDayAvg, setCurrentDayAvg] = useState("Temp Avg");
+
+  //next Day States
+  const [nextDay, setNextDay] = useState("Next Day");
+  const [nextDayWeatherImage, setNextDayWeatherImage] =
+    useState("Animated Image");
+  const [nextDayWeather, setNextDayWeatehr] = useState("Expected Weather");
+  const [nextDayAvg, setNextDayAvg] = useState("Temp Avg");
+
+  //2 future day States
+  const [nnextDay, setNNextDay] = useState("Next Day");
+  const [nnextDayWeatherImage, setNNextDayWeatherImage] =
+    useState("Animated Image");
+  const [nnextDayWeather, setNNextDayWeatehr] = useState("Expected Weather");
+  const [nnextDayAvg, setNNextDayAvg] = useState("Temp Avg");
+
+  //3 future day States
+  const [nnnextDay, setNNNextDay] = useState("Next Day");
+  const [nnnextDayWeatherImage, setNNNextDayWeatherImage] =
+    useState("Animated Image");
+  const [nnnextDayWeather, setNNNextDayWeatehr] = useState("Expected Weather");
+  const [nnnextDayAvg, setNNNextDayAvg] = useState("Temp Avg");
 
   return (
     <div className="h-screen pb-16 flex flex-row">
@@ -19,31 +50,31 @@ export default function Home() {
       <div className="flex-1 grid grid-col-2 grid-flow-col gap-4 p-5 bg-slate-300">
         <div className="bg-slate-400 rounded-lg flex-1 flex-column">
           <div className="h-2/3 mt-20 bg-indigo-400 flex-1 flex-column">
-            <div className="p-8">Current Day</div>
-            <div className="p-8 h-1/3">Animated image</div>
-            <div className="p-8">Weather that day</div>
-            <div className="p-8">Temp Avg</div>
+            <div className="p-8">{currentDay}</div>
+            <div className="p-8 h-1/3">{currentDayWeatherImage}</div>
+            <div className="p-8">{currentDayWeather}</div>
+            <div className="p-8">{currentDayAvg}</div>
           </div>
         </div>
         <div className="bg-slate-400 rounded-lg flex-1 flex-column">
           <div className="grid grid-col-3 grid-flow-col gap-3 mt-20 h-1/3 pb-2">
             <div className="pl-2 bg-violet-900">
-              <div className="p-3">Next Day</div>
-              <div className="p-3 h-1/3">Animated image</div>
-              <div className="p-3">Expected Weather</div>
-              <div className="p-3">Temp Avg</div>
+              <div className="p-3">{nextDay}</div>
+              <div className="p-3 h-1/3">{nextDayWeatherImage}</div>
+              <div className="p-3">{nextDayWeather}</div>
+              <div className="p-3">{nextDayAvg}</div>
             </div>
             <div className="bg-violet-700">
-              <div className="p-3">Next Day</div>
-              <div className="p-3 h-1/3">Animated image</div>
-              <div className="p-3">Expected Weather</div>
-              <div className="p-3">Temp Avg</div>
+              <div className="p-3">{nnextDay}</div>
+              <div className="p-3 h-1/3">{nnextDayWeatherImage}</div>
+              <div className="p-3">{nnextDayWeather}</div>
+              <div className="p-3">{nnextDayAvg}</div>
             </div>
             <div className="pr-2 bg-violet-500">
-              <div className="p-3">Next Day</div>
-              <div className="p-3 h-1/3">Animated image</div>
-              <div className="p-3">Expected Weather</div>
-              <div className="p-3">Temp Avg</div>
+              <div className="p-3">{nnnextDay}</div>
+              <div className="p-3 h-1/3">{nnnextDayWeatherImage}</div>
+              <div className="p-3">{nnnextDayWeather}</div>
+              <div className="p-3">{nnnextDayAvg}</div>
             </div>
           </div>
           <div className="h-1/3 bg-indigo-700">
--- src/utils/apiSearch.js
+++ src/utils/apiSearch.js
@@ -2,10 +2,13 @@
 //and then inject the lat and long into a new fetch for the towns weather
 //want to ask the users location to get the lat and long that way eventually
 export default function ApiSearch(data, type) {
+  //data will either contain a city name with a type of false
+  //or data will contain coords with a type of true
   const apiLocationCity = `http://api.openweathermap.org/geo/1.0/direct?q=${data}&limit=5&appid=${process.env.REACT_APP_API_KEY}`;
   const lat = data[0];
   const long = data[1];
-  const apiLocationLATLONG = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`;
+  //this also gives a lot of data https://api.openweathermap.org/data/2.5/forecast?
+  const apiLocationLATLONG = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`;
   console.log(data, type);
   const findLocation = (apiData, bool) => {
     if (bool) {
@@ -22,14 +25,14 @@ export default function ApiSearch(data, type) {
             alert("That is not a valid city");
           }
           console.log(data);
+
+          return data;
         })
         .catch((error) => {
           console.error("There was a problem finding that city", error);
         });
     }
   };
-
-  return type
-    ? findLocation(apiLocationLATLONG)
-    : findLocation(apiLocationCity);
+  type ? findLocation(apiLocationLATLONG) : findLocation(apiLocationCity);
+  return;
 }
