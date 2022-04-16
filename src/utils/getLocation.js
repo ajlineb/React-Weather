de1230a04bee657ed0724bba7daa5e67e3a4f1b6
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-15 20:12:59 -0400

    minor css changes to location button

--- src/utils/getLocation.js
+++ src/utils/getLocation.js
@@ -30,7 +30,7 @@ export default function GetLocation() {
   };
 
   return (
-    <div className="p-1">
+    <div className="p-1 basis-1/3 flex justify-left items-center">
       <button
         className="px-3 py-2 text-white font-bold rounded-lg bg-indigo-700 hover:bg-violet-700 active:bg-violet-700 focus:outline-none focus:ring focus:ring-indigo-700 shrink h-10"
         onClick={getLocation}
