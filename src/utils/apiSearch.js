7b92655b06925556cfde834e5dcee9e488ab098e
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-15 20:10:57 -0400

    successfully storing data in the localstorage per city name

--- src/utils/apiSearch.js
+++ src/utils/apiSearch.js
@@ -7,32 +7,52 @@ export default function ApiSearch(data, type) {
   const apiLocationCity = `http://api.openweathermap.org/geo/1.0/direct?q=${data}&limit=5&appid=${process.env.REACT_APP_API_KEY}`;
   const lat = data[0];
   const long = data[1];
-  //this also gives a lot of data https://api.openweathermap.org/data/2.5/forecast?
-  const apiLocationLATLONG = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`;
+  //this also gives a lot of data https://api.openweathermap.org/data/2.5/forecast?  or https://api.openweathermap.org/data/2.5/onecall?
+  const apiLocationLATLONG = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`;
   console.log(data, type);
-  const findLocation = (apiData, bool) => {
-    if (bool) {
-    } else {
-      fetch(apiData)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("Network Was interuppted!");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          if (data.length < 1) {
-            alert("That is not a valid city");
-          }
-          console.log(data);
 
-          return data;
-        })
-        .catch((error) => {
-          console.error("There was a problem finding that city", error);
-        });
-    }
+  //used if location services were used
+  const findLocation = (apiData) => {
+    fetch(apiData)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network Was interuppted!");
+        }
+        return res.json();
+      })
+      .then((d) => {
+        if (d.length < 1) {
+          alert("That is not a valid city");
+        }
+        console.log(d);
+        localStorage.setItem(`${d.city.name}`, JSON.stringify(d));
+        return d;
+      })
+      .catch((error) => {
+        console.error("There was a problem finding that city", error);
+      });
   };
-  type ? findLocation(apiLocationLATLONG) : findLocation(apiLocationCity);
+
+  //for finding the lat/long
+  const findLocationCity = (apiData) => {
+    fetch(apiData)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network Was interuppted!");
+        }
+        return res.json();
+      })
+      .then((d) => {
+        if (d.length < 1) {
+          alert("That is not a valid city");
+        }
+        console.log(d);
+        return d;
+      })
+      .catch((error) => {
+        console.error("There was a problem finding that city", error);
+      });
+  };
+  type ? findLocation(apiLocationLATLONG) : findLocationCity(apiLocationCity);
   return;
 }
