c7333195459f49f2a2518ca37c0df412684c38b7
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-20 21:04:09 -0400

    working on the how the weather gets updated

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import data from "../utils/imageBase";
 
 export default function Weather() {
+  const [key, setKey] = useState(null);
   const [value, setValue] = useState(null);
   const [storage, setStorage] = useState(null);
 
   const handleInfo = () => {};
   //will use this to rerender weather info on the page as the local storage has changed
-  useEffect(() => {}, [storage]);
+  useEffect(() => {}, []);
   return;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -86,6 +86,7 @@ export default function Home() {
   return (
     <div className="h-screen pb-16 flex flex-row">
       <Sidetabs />
+
       <div className="flex-1 grid grid-col-2 grid-flow-col gap-4 p-5 bg-slate-300">
         <div className="bg-slate-400 rounded-lg flex-1 flex-column">
           <div className="h-2/3 mt-20 bg-indigo-400 flex-1 flex-column">
