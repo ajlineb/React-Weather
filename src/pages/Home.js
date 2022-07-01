1a798ead2dd4e1c60525f85bc51e0310bd65239f
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-06-30 22:13:51 -0400

    the weather is now in 7 columns representing 7 days of forcast including the present day

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -111,66 +111,73 @@ export default function Home() {
   return (
     <>
       <Header setSearchResults={setSearchResults} />
-
-      {!searchResults || searchResults === undefined ? (
-        <div>
-          <h3>
-            <b>No Data Found</b>
-          </h3>
-        </div>
-      ) : (
-        searchResults.daily.map((result, id) => {
-          //this is where the goods is going!!!
-          return (
-            <div
-              key={id}
-              className="flex-1 grid grid-col-2 grid-flow-col gap-4 p-5 bg-slate-300"
-            >
-              <div className="bg-slate-400 rounded-lg flex-1 flex-column">
-                <div className="text-lg font-bold flex justify-center items-center"></div>
-                {/* place the current day info here */}
-                <div className="h-2/3 mt-20 bg-indigo-400 flex-1 flex-column">
-                  <div className="p-8">
-                    Todays Date: {convertTime(result.dt)}
+      <div className="grid grid-col-7 grid-flow-col p-2 gap-4 bg-green-200">
+        {!searchResults || searchResults === undefined ? (
+          <div>
+            <h3>
+              <b>No Data Found</b>
+            </h3>
+          </div>
+        ) : (
+          searchResults.daily.map((result, id) => {
+            //this is where the goods is going!!!
+            return (
+              <div
+                key={id}
+                className="flex-1 grid grid-col-2 grid-flow-col bg-slate-300"
+              >
+                <div className="bg-slate-400 rounded-lg mb-16 flex-1 flex-column">
+                  <div className="text-lg font-bold flex justify-center items-center"></div>
+                  {/* place the current day info here */}
+                  <div className="h-2/3 bg-indigo-400 flex-1 flex-column">
+                    <div className="p-8">
+                      Todays Date: {convertTime(result.dt)}
+                    </div>
+                    <div className="p-8 h-1/3">
+                      {handleImage(result.weather[0].description)}
+                      <p>
+                        Weather description: {result.weather[0].description}
+                      </p>
+                    </div>
+                    <div className="p-8">
+                      Current Weather: {result.weather[0].main}
+                    </div>
+                    <div className="p-8">
+                      Temp: {Math.floor(result.temp.day - 273)}°C or{" "}
+                      {Math.floor(((result.temp.day - 273) * 9) / 5 + 32)}°F
+                    </div>
                   </div>
-                  <div className="p-8 h-1/3">
-                    {handleImage(result.weather[0].description)}
-                    <p>Weather description: {result.weather[0].description}</p>
-                  </div>
-                  <div className="p-8">
-                    Current Weather: {result.weather[0].main}
-                  </div>
-                  <div className="p-8">
-                    Temp: {Math.floor(result.temp.day - 273)}°C or{" "}
-                    {Math.floor(((result.temp.day - 273) * 9) / 5 + 32)}°F
+                  <div className="bg-slate-400 rounded-lg flex-1 flex-column">
+                    <div className="h-1/3 bg-indigo-700">
+                      <div className="p-3 h-1/4">
+                        Alt info for the current day
+                      </div>
+                      <div className="p-3">
+                        Wind info:{" "}
+                        {checkDirection(result.wind_deg) +
+                          "ward " +
+                          result.wind_gust +
+                          " Gust " +
+                          result.wind_speed +
+                          " Wind Speed "}
+                      </div>
+                      <div className="p-3">
+                        Extra info about the day maybe holiday?
+                      </div>
+                      <h2 className="p-3">
+                        UV index:{" "}
+                        <span className={uvColor(result.uvi)}>
+                          {result.uvi}
+                        </span>
+                      </h2>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="bg-slate-400 rounded-lg flex-1 flex-column">
-                <div className="h-1/3 bg-indigo-700">
-                  <div className="p-3 h-1/4">Alt info for the current day</div>
-                  <div className="p-3">
-                    Wind info:{" "}
-                    {checkDirection(result.wind_deg) +
-                      "ward " +
-                      result.wind_gust +
-                      " Gust " +
-                      result.wind_speed +
-                      " Wind Speed "}
-                  </div>
-                  <div className="p-3">
-                    Extra info about the day maybe holiday?
-                  </div>
-                  <h2 className="p-3">
-                    UV index:{" "}
-                    <span className={uvColor(result.uvi)}>{result.uvi}</span>
-                  </h2>
-                </div>
-              </div>
-            </div>
-          );
-        })
-      )}
+            );
+          })
+        )}
+      </div>
     </>
   );
 }
