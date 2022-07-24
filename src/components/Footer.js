2ea462e4e0442d222c998b8b7c16ba94931d6045
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-07-24 16:05:17 -0400

    added source code link in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,16 @@ import React, { useState } from "react";
 export default function Footer() {
   return (
     <div className="fixed bottom-0 left-1/2 -translate-x-1/2 font-bold text-center w-full bg-white h-16">
-      <h1 className="">Created with ❤️ By Anthony Linebaugh</h1>
+      <h1 className="">
+        Created with ❤️ By Anthony Linebaugh
+        <a href="https://github.com/ajlineb/React-Weather">
+          {" "}
+          <span className="text-indigo-500 hover:text-purple-400">
+            [Source Code]
+          </span>
+        </a>
+      </h1>
+
       <h1>© 2022</h1>
     </div>
   );
